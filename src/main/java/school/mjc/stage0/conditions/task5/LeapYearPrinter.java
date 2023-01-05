99f2ae843688a069ea0eb314e02e5fa81314ae2c
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-05 23:47:31 +0100

    Performed RangeNumbersPrinter task

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        String leap = String.valueOf((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+        switch (leap) {
+            case "true":
+                System.out.println("leap");
+                break;
+            case "false":
+                System.out.println("not leap");
+                break;
+        }
     }
 }
